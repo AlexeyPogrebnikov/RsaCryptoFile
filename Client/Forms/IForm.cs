88af83258e5a4c8e3c56392db35837cc1c383a19
alexey.pogrebnikov <wpsite@gmail.com>
2020-10-14 19:38:03 +0300

    Build fix.

--- Client/Forms/IForm.cs
+++ Client/Forms/IForm.cs
@@ -4,7 +4,6 @@ using CryptoFile.Client.Configuration;
 
 namespace CryptoFile.Client.Forms {
 	public interface IForm : IDisposable {
-		event FormClosedEventHandler FormClosing;
 		Language Language { get; set; }
 		DialogResult DialogResult { get; set; }
 		DialogResult ShowDialog();
